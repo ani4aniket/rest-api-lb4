3797ed6c8d215e6ef442268312e3919a649305b0
Aniket Kumar <aniketkumar049@gmail.com>
2020-05-21 12:02:10 +0530

    hasMany, belongsTo and hasOne added

---
+++ src/models/student.model.ts
@@ -0,0 +1,47 @@
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
+import {Address, AddressRelations} from './address.model';
+import {Department} from './department.model';
+
+@model()
+export class Student extends Entity {
+  @property({
+    type: 'number',
+    generated: true,
+    id: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'number',
+  })
+  courseId?: number;
+
+  @belongsTo(() => Department)
+  departmentId: number;
+
+  @hasOne(() => Address)
+  address: Address;
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+  address?: AddressRelations;
+}
+
+export type StudentWithRelations = Student & StudentRelations;
